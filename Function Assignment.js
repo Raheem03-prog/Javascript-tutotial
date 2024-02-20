23507a29fd1bf568c3e1e9464af2d88bcc2ea28a
Raheem03-prog <147347937+Raheem03-prog@users.noreply.github.com>
2024-02-20 00:52:37 -0800

    Update Function Assignment.js

--- Function Assignment.js
+++ Function Assignment.js
@@ -20,6 +20,33 @@ function returnMinValue(numberArray){
     return lowestNumber; 
 }
 
-let myArray =[];
+let myArray =[1234,56789, 5657,5766,9999,90];
 
-console.log (returnMinValue(myArray))
+//console.log (returnMinValue(myArray))
+
+function getFrequency(array, number){
+    let frequency = 0;
+
+    for(let x of array){
+        if (x === number){
+            frequency += 1;
+        }
+    }
+    return frequency;
+}
+
+
+
+function maxValue(numberArray){
+    const highestValue = [0];
+    let results = numbers[0];
+
+    for( let i = 0; i < highestValue.length; i++){
+        if ( numbers[i] > results){
+            results = numbers[i];
+        }
+    }
+    return results
+}
+
+console.log(maxValue(myArray));
